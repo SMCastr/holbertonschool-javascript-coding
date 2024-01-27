450a3f2863004fb42802812a3038ea35c8dcaeaa
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 22:02:31 -0400

    fc space blankc fixxxxxxxxxx

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -9,26 +9,24 @@ const str = process.argv[3];
  * @param {string} filePath - The file path.
  * @param {string} content - The string to write to the file.
  */
+
 function writeToFile(filePath, content) {
-	// Write the content to the file in utf-8
+
 	fs.writeFile(filePath, content, "utf-8", (error) => {
 		if (error) {
-			// If an error occurred during writing, print the error object
+
 			console.error(error);
 		}
 	});
 }
 
-// Check if the correct number of command line arguments is provided
 if (process.argv.length !== 4) {
-	// If not, print an error message and exit
+
 	console.error("Usage: ./1-writeme.js <file-path> <content>");
 	process.exit(1);
 }
 
-// Get the file path and content from the command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Call the writeToFile function with the provided file path and content
 writeToFile(filePath, content);
--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,28 +6,23 @@ const request = require('request');
  * getStatusCode - Displays the status code of a GET request.
  * @param {string} url - The URL to request (GET).
  */
+
 function getStatusCode(url) {
-	// Use the request module to make a GET request to the specified URL
+
 	request.get(url, (error, response) => {
 		if (error) {
-			// If an error occurred during the request, print the error object
 			console.error(error);
 		} else {
-			// Print the status code
 			console.log(`code: ${response.statusCode}`);
 		}
 	});
 }
 
-// Check if the correct number of command line arguments is provided
 if (process.argv.length !== 3) {
-	// If not, print an error message and exit
 	console.error('Usage: ./2-statuscode.js <url>');
 	process.exit(1);
 }
 
-// Get the URL from the command line arguments
 const url = process.argv[2];
 
-// Call the getStatusCode function with the provided URL
 getStatusCode(url);
