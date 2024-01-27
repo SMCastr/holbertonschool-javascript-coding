902c8a23356e2bfa861f0da8d7b171d5485e0a08
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 22:05:29 -0400

    try cod fucnt

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,32 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const nameFile = process.argv[2];
-const str = process.argv[3];
-
-/**
- * writeToFile - Writes a string to a file.
- * @param {string} filePath - The file path.
- * @param {string} content - The string to write to the file.
- */
-
-function writeToFile(filePath, content) {
-
-	fs.writeFile(filePath, content, "utf-8", (error) => {
-		if (error) {
-
-			console.error(error);
-		}
-	});
-}
-
-if (process.argv.length !== 4) {
-
-	console.error("Usage: ./1-writeme.js <file-path> <content>");
-	process.exit(1);
-}
-
 const filePath = process.argv[2];
-const content = process.argv[3];
+const string = process.argv[3];
 
-writeToFile(filePath, content);
+fs.writeFile(filePath, string, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
