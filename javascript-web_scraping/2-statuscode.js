dceaccc0f9963b018bd86210b184d25258fdd67e
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 22:06:02 -0400

    update statu code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,28 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-/**
- * getStatusCode - Displays the status code of a GET request.
- * @param {string} url - The URL to request (GET).
- */
-
-function getStatusCode(url) {
-
-	request.get(url, (error, response) => {
-		if (error) {
-			console.error(error);
-		} else {
-			console.log(`code: ${response.statusCode}`);
-		}
-	});
-}
-
-if (process.argv.length !== 3) {
-	console.error('Usage: ./2-statuscode.js <url>');
-	process.exit(1);
-}
-
 const url = process.argv[2];
 
-getStatusCode(url);
+request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:', res.statusCode);
+  }
+}
+);
