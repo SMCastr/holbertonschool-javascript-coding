8c702b86c2c11e59e6dc133cd4fdc16d43108d5b
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 21:54:45 -0400

    strcyt

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -9,20 +9,21 @@ const fs = require('fs');
 
 
 function readFileContent(filePath) {
-  fs.readFile(filePath, 'utf-8', (error, data) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(data);
-    }
-  });
+	fs.readFile(filePath, 'utf-8', (error, data) => {
+		if (error) {
+			console.error(error);
+		} else {
+			console.log(data);
+		}
+	});
 }
 
 if (process.argv.length !== 3) {
-  console.error('Usage: ./0-readme.js <file-path>');
-  process.exit(1);
+	console.error('Usage: ./0-readme.js <file-path>');
+	process.exit(1);
 }
 
 const filePath = process.argv[2];
+
 
 readFileContent(filePath);
