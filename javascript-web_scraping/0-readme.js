228e6bf8ba2cb8926acdd459aebe05c419597a3d
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 21:33:37 -0400

    redy

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,30 +1,25 @@
 #!/usr/bin/node
 
-
 const fs = require('fs');
 
-
 /**
- * readFileContent - Reads and prints the content of a file.
- * @param {string} filePath - The file path.
+ * readFileContent - Lee e imprime el contenido de un archivo.
+ * @param {string} filePath - La ruta del archivo.
  */
-
-
 function readFileContent(filePath) {
-	fs.readFile(filePath, 'utf-8', (error, data) => {
-		if (error) {
-			console.error(error);
-		} else {
-			console.log(data);
-		}
-	});
+  fs.readFile(filePath, 'utf-8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(data);
+    }
+  });
 }
 
 if (process.argv.length !== 3) {
-	console.error('Usage: ./0-readme.js <file-path>');
-	process.exit(1);
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
 }
-
 
 const filePath = process.argv[2];
 
