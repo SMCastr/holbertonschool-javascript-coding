fe032304205d6c10cc54a7a982cfad6733906420
SMCastr <mariesweetycastro@gmail.com>
2024-01-26 21:12:55 -0400

    mod struc

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+
+const fs = require('fs');
 const nameFile = process.argv[2];
+
 
 /**
  * readFileContent - Reads and prints the content of a file.
  * @param {string} filePath - The file path.
  */
+
 function readFileContent(filePath) {
 	// Read the content of the file in utf-8
-	fs.readFile(filePath, "utf-8", (error, data) => {
+	fs.readFile(filePath, 'utf-8', (error, data) => {
 		if (error) {
 			// If an error occurred during the reading, print the error object
 			console.error(error);
@@ -23,7 +26,7 @@ function readFileContent(filePath) {
 // Check if the correct number of command line arguments is provided
 if (process.argv.length !== 3) {
 	// If not, print an error message and exit
-	console.error("Usage: ./0-readme.js <file-path>");
+	console.error('Usage: ./0-readme.js <file-path>');
 	process.exit(1);
 }
 
